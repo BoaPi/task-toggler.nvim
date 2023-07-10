913c9c36706057998bd1ceeb928e629c4f20331d
boapi <tobias.weyer1205@gmail.com>
2023-07-10 22:14:14 +0200

    add capturing of unchecked task with treesitter

--- lua/tasktoggler/init.lua
+++ lua/tasktoggler/init.lua
@@ -4,7 +4,32 @@ local M = {}
 M.setup = function()
 	print("setup is working")
 
-	vim.api.nvim_create_user_command("TaskTogglerCheck", function() end, {})
+	vim.api.nvim_create_user_command("TaskTogglerCheck", function()
+		local parser = vim.treesitter.get_parser(0, "markdown", {})
+		local tree = parser:parse()[1]
+		local root = tree:root()
+
+		local query = vim.treesitter.query.parse(
+			"markdown",
+			[[
+			(task_list_marker_unchecked) @marker]]
+		)
+
+		local currentLine = unpack(vim.api.nvim_win_get_cursor(0)) - 1
+
+		for _, captures, _ in query:iter_captures(root, line) do
+			local row1, col1, row2, col2 = captures:range()
+			print(row1, row2, col1, col2)
+			if row1 == currentLine then
+				print(captures)
+				print(currentLine)
+			end
+		end
+
+		print("end")
+
+		-- print(vim.treesitter.query.get_node_text(node))
+	end, {})
 	vim.api.nvim_create_user_command("TaskTogglerUncheck", function() end, {})
 	vim.api.nvim_create_user_command("TaskTogglerToggle", function() end, {})
 end
