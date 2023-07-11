bf75ef87ba45a420042abe6cd9f614a0df9952fe
boapi <tobias.weyer1205@gmail.com>
2023-07-11 21:09:35 +0200

    update to first working version and clean up

--- lua/tasktoggler/init.lua
+++ lua/tasktoggler/init.lua
@@ -1,62 +1,38 @@
 -- setup module
 local M = {}
 
-M.setup = function()
-	print("setup is working")
+local checked = "x"
+local unchecked = " "
 
+M.setup = function()
 	vim.api.nvim_create_user_command("TaskTogglerCheck", function()
+		-- get parser of current buffer and parse out the tree
+		-- finally get the root of the tree
 		local parser = vim.treesitter.get_parser(0, "markdown", {})
 		local tree = parser:parse()[1]
 		local root = tree:root()
 
+		-- create parsed query for unchecked
 		local query = vim.treesitter.query.parse(
 			"markdown",
 			[[
 			(task_list_marker_unchecked) @marker]]
 		)
 
+		-- get current line of the cursor to determine which task should be toggled
 		local currentLine = unpack(vim.api.nvim_win_get_cursor(0)) - 1
 
-		for _, captures, _ in query:iter_captures(root, line) do
-			local row1, col1, row2, col2 = captures:range()
-			print(row1, row2, col1, col2)
+		for _, capture, _ in query:iter_captures(root, line) do
+			local row1, col1 = capture:range()
+
+			-- if current line matches the capture row, check task
 			if row1 == currentLine then
-				print(captures)
-				print(currentLine)
+				vim.api.nvim_buf_set_text(0, row1, col1 + 1, row1, col1 + 2, { checked })
 			end
 		end
-
-		print("end")
-
-		-- print(vim.treesitter.query.get_node_text(node))
 	end, {})
 	vim.api.nvim_create_user_command("TaskTogglerUncheck", function() end, {})
 	vim.api.nvim_create_user_command("TaskTogglerToggle", function() end, {})
 end
 
 return M
-
--- ToggleToDo
--- function to check or un-check a To-Do in .md filesystem
---vim.api.nvim_create_user_command("ToggleToDo", function()
---local line = vim.api.nvim_get_current_line()
---local newLine
---
----- pattern to match start of a to-do with "*" or "-"
---local checkPattern = "(%s*[%*|%-] )%[[x]%](.*)"
---local unCheckPattern = "(%s*[%*|%-] )%[[%s]%](.*)"
---
----- create matches for each pattern
---local matchCheck = string.match(line, checkPattern)
---local matchUnCheck = string.match(line, unCheckPattern)
---
----- if one pattern matches replace line with new state
---if matchCheck then
---newLine = string.gsub(line, checkPattern, "%1[ ]%2")
---elseif matchUnCheck then
---newLine = string.gsub(line, unCheckPattern, "%1[x]%2")
---end
---
----- replace line with new toggled line
---vim.api.nvim_set_current_line(newLine)
---end, {})
